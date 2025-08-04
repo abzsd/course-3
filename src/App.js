242978fc323b4465def2dc27f59af8bc3dbbd7ee
curiousmuffin <abhaykambley1@gmail.com>
2025-08-04 20:51:26 +0530

    navbar using routes, route, link

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import './App.css';
 // import ModeToggler from './ModeToggler';
-import MealsProvider from "./providers/MealsProvider";
-import MealsList from "./components/MealsList";
-import Counter from "./components/Counter";
+// import MealsProvider from "./providers/MealsProvider";
+// import MealsList from "./components/MealsList";
+// import Counter from "./components/Counter";
+import Homepage from './Homepage';
+import AboutMe from "./AboutMe";
+import { Routes, Route, Link} from 'react-router-dom';
 
-const randNum = () => Math.floor(Math.random() * 100) + 1;
+// const randNum = () => Math.floor(Math.random() * 100) + 1;
 
 function App() {
   return (
-    <div>
-      <MealsProvider>
-        <MealsList />
-        <Counter />
-      </MealsProvider>
+    <div className='App'>
+      <nav className='nav'>
+        <Link to="/" className="nav-item">Homepage</Link>
+        <Link to="/about-me" className="nav-item">About Me</Link>
+      </nav>
+      <Routes>
+        <Route path='/' element={<Homepage />} />
+        <Route path='/about-me' element={<AboutMe />} />
+      </Routes>
     </div>
   );
 }
